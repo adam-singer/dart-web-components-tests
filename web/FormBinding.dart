eb5046f6d78f3a15c8a674ea2e88e35d23d43419
Seth Ladd <sethladd@sethladd-macbookpro.local>
2012-11-14 11:21:08 +0100

    simplify form binding

--- web/FormBinding.dart
+++ web/FormBinding.dart
@@ -1,12 +1,5 @@
 import 'dart:math';
 
-String superlative;
+String superlative = '';
 
-List<String> alternatives = const ['wicked cool', 'sweet', 'fantastic'];
-Random random = new Random();
-
-main() {
-  superlative = 'awesome';
-}
-
-changeIt(e) => superlative = alternatives[random.nextInt(3)];
+main() { }
